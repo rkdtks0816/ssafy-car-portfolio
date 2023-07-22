ea6428792b0a611f055bcf5eeb229670fe6bdc20
rkdtks0816 <rkdtks0816@naver.com>
2023-07-22 14:49:31 +0900

    editportfolio1

--- app.js
+++ app.js
@@ -1,102 +1,143 @@
 const car = document.querySelector(".car");
 const carBaseImgUrl = "./img/car";
-const aboutBtn = document.querySelector(".about-btn");
-const skillBtn = document.querySelector(".skill-btn");
-const projectBtn = document.querySelector(".project-btn");
-const contactBtn = document.querySelector(".contact-btn");
-const infoModal = document.querySelector("info-modal")
+
+const arrowUpController = document.querySelector(".arrow-up-controller");
+const arrowLeftController = document.querySelector(".arrow-left-controller");
+const arrowRightController = document.querySelector(".arrow-right-controller");
+const arrowDownController = document.querySelector(".arrow-down-controller");
+
+const aboutArea = document.querySelector(".about-area");
+const skillArea = document.querySelector(".skill-area");
+const projectArea = document.querySelector(".project-area");
+const contactArea = document.querySelector(".contact-area");
+
+const infoModal = document.getElementById("infoModal");
+const bootstrapModal = new bootstrap.Modal(infoModal, {});
 const modalTatle = document.querySelector(".modal-title");
 const modalBody = document.querySelector(".modal-body");
 
 const carPosition = {
-  top: 55,
+  top: 50,
   left: 50,
 };
 
+let modalTatleText = "";
+let modalBodyHTML = "";
+
+const checkCarPosition = function () {
+;  if (carPosition.top < 35 && carPosition.left < 33) {
+    aboutArea.style.backgroundColor = "#CBCBCB"
+    modalTatleText = "ABOUT";
+    modalBodyHTML = `
+      <h1> 송강산 </h1>
+      <div>전북대학교 전자공학부 졸업</div>
+      <div>SSAFY Embedded Track</div>
+    `;
+    modalTatle.textContent = modalTatleText;
+    modalBody.innerHTML = modalBodyHTML;
+    bootstrapModal.show();
+  } else if (carPosition.top < 35 && carPosition.left > 67) {
+    modalTatleText = "SKILL";
+    modalBodyHTML = `
+      <div>PYTHON</div>
+      <div>JAVASCRIPT</div>
+      <div>C++</div>
+    `;
+    modalTatle.textContent = modalTatleText;
+    modalBody.innerHTML = modalBodyHTML;
+    bootstrapModal.show();
+  } else if (carPosition.top > 65 && carPosition.left < 33) {
+    modalTatleText = "PROJECT";
+    modalBodyHTML = `
+      <div>Portfolio</div>
+      <div>Mincoding</div>
+      <div>RC카 제작</div>
+    `;
+    modalTatle.textContent = modalTatleText;
+    modalBody.innerHTML = modalBodyHTML;
+    bootstrapModal.show();
+  } else if (carPosition.top > 65 && carPosition.left > 67) {
+    modalTatleText = "CONTACT";
+    modalBodyHTML = `
+      <div>email : rkdtks0816@naver.com</div>
+      <div>phone : 010-5054-3624</div>
+      <div>instagram : __rkdtks_</div>
+    `;
+
+    modalTatle.textContent = modalTatleText;
+    modalBody.innerHTML = modalBodyHTML;
+    bootstrapModal.show();
+  } else {
+    aboutArea.style.backgroundColor = "#FFF"
+    bootstrapModal.hide();
+  }
+};
+
+arrowUpController.addEventListener("click", function (e) {
+  car.style.backgroundImage = `url(${carBaseImgUrl}Top.png)`;
+  if (carPosition.top >= 12) {
+    carPosition.top--;
+  }
+
+  car.style.top = String(carPosition.top) + "%";
+
+  checkCarPosition();
+});
+arrowLeftController.addEventListener("click", function (e) {
+  car.style.backgroundImage = `url(${carBaseImgUrl}Left.png)`;
+  if (carPosition.left >= 7) {
+    carPosition.left -= 2;
+  }
+
+  car.style.left = String(carPosition.left) + "%";
+
+  checkCarPosition();
+});
+arrowRightController.addEventListener("click", function (e) {
+  car.style.backgroundImage = `url(${carBaseImgUrl}Right.png)`;
+  if (carPosition.left <= 93) {
+    carPosition.left += 2;
+  }
+
+  car.style.left = String(carPosition.left) + "%";
+
+  checkCarPosition();
+});
+arrowDownController.addEventListener("click", function (e) {
+  car.style.backgroundImage = `url(${carBaseImgUrl}Bottom.png)`;
+  if (carPosition.top <= 88) {
+    carPosition.top++;
+  }
+
+  car.style.top = String(carPosition.top) + "%";
+
+  checkCarPosition();
+});
+
 document.addEventListener("keydown", function (e) {
-    if (e.code === "ArrowUp") {
-      car.style.backgroundImage = `url(${carBaseImgUrl}Top.png)`;
-      if (carPosition.top >= 0) {
-        carPosition.top--;
-      }
-      else {
-        carPosition.top++;
-      }
-    } else if (e.code === "ArrowDown") {
-      car.style.backgroundImage = `url(${carBaseImgUrl}Bottom.png)`;
-      if (carPosition.top <= 100) {
-          carPosition.top++;
-        }
-        else {
-          carPosition.top--;
-        }
-    } else if (e.code === "ArrowLeft") {
-      car.style.backgroundImage = `url(${carBaseImgUrl}Left.png)`;
-      if (carPosition.left >= 0) {
-          carPosition.left--;
-        }
-        else {
-          carPosition.left++;
-        }
-    } else if (e.code === "ArrowRight") {
-      car.style.backgroundImage = `url(${carBaseImgUrl}Right.png)`;
-      if (carPosition.left <= 100) {
-          carPosition.left++;
-        }
-        else {
-          carPosition.left--;
-        }
+  if (e.code === "ArrowUp") {
+    car.style.backgroundImage = `url(${carBaseImgUrl}Top.png)`;
+    if (carPosition.top >= 12) {
+      carPosition.top--;
     }
+  } else if (e.code === "ArrowLeft") {
+    car.style.backgroundImage = `url(${carBaseImgUrl}Left.png)`;
+    if (carPosition.left >= 7) {
+      carPosition.left -= 2;
+    }
+  } else if (e.code === "ArrowRight") {
+    car.style.backgroundImage = `url(${carBaseImgUrl}Right.png)`;
+    if (carPosition.left <= 93) {
+      carPosition.left += 2;
+    }
+  } else if (e.code === "ArrowDown") {
+    car.style.backgroundImage = `url(${carBaseImgUrl}Bottom.png)`;
+    if (carPosition.top <= 88) {
+      carPosition.top++;
+    }
+  }
   car.style.top = String(carPosition.top) + "%";
   car.style.left = String(carPosition.left) + "%";
 
-  if (carPosition.top < 40 &&
-    carPosition.left < 35 ){
-    aboutBtn.click();
-  } else if (carPosition.top < 40 &&
-    carPosition.left > 65){
-    skillBtn.click();
-  } else if (carPosition.top > 60 &&
-    carPosition.left < 35){
-    projectBtn.click();
-  } else if (carPosition.top > 60 &&
-    carPosition.left > 65 ){
-    contactBtn.click();
-  }
-});
-
-aboutBtn.addEventListener("click", function (e) {
-  modalTatle.textContent = aboutBtn.textContent;
-  modalBody.innerHTML = `
-    <h1> 송강산 </h1>
-    <div>전북대학교 전자공학부 졸업</div>
-    <div>SSAFY Embedded Track</div>
-  `;
-});
-
-skillBtn.addEventListener("click", function (e) {
-  modalTatle.textContent = skillBtn.textContent;
-  modalBody.innerHTML = `
-    <div>PYTHON</div>
-    <div>JAVASCRIPT</div>
-    <div>C++</div>
-  `;
-});
-
-projectBtn.addEventListener("click", function (e) {
-  modalTatle.textContent = projectBtn.textContent;
-  modalBody.innerHTML = `
-    <div>Portfolio</div>
-    <div>Mincoding</div>
-    <div>RC카 제작</div>
-  `;
-});
-
-contactBtn.addEventListener("click", function (e) {
-  modalTatle.textContent = contactBtn.textContent;
-  modalBody.innerHTML = `
-    <div>email : rkdtks0816@naver.com</div>
-    <div>phone : 010-5054-3624</div>
-    <div>instagram : __rkdtks_</div>
-  `;
+  checkCarPosition();
 });
